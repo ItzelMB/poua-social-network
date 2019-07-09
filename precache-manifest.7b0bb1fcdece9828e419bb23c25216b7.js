fae63a1fa0d30a4441cd48c6fa94a6771da865b1
ItzelMB <it.mobec@gmail.com>
2019-07-09 08:59:46 -0500

    Updates

---
+++ precache-manifest.7b0bb1fcdece9828e419bb23c25216b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ea78b2adb4fdebcd20284e5beaba7c",
+    "url": "/poua-social-network/index.html"
+  },
+  {
+    "revision": "7ed7a4374219e07b2e17",
+    "url": "/poua-social-network/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9e5e36c9eb07827c8740",
+    "url": "/poua-social-network/static/js/2.a746d180.chunk.js"
+  },
+  {
+    "revision": "7ed7a4374219e07b2e17",
+    "url": "/poua-social-network/static/js/main.b0e8dac3.chunk.js"
+  },
+  {
+    "revision": "decb3ce0b6406596c72f",
+    "url": "/poua-social-network/static/js/runtime~main.113b0031.js"
+  }
+]);
